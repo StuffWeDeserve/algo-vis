f5b24f70d04880f8f0b838792ef56a7eaf136630
sakshaat_dc <sakshaat_dc@hotmail.com>
2018-07-05 16:41:20 -0400

    Fixed visualizer, fixed deletion issue with generator, added slider and fixed other issures with list keys

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import "./Visualizer.css"
 import ListElement from './ListElement';
+import shortid from 'shortid';
 
 class Visualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      max : Math.max(...this.props.list)
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let newMax = Math.max(...nextProps.list);
+    // You don't have to do this check first, but it can help prevent an unneeded render
+    if (newMax !== this.state.max) {
+      this.setState({ max: newMax });
+    }
+  }
+
   render() {
+
+    const { max } = this.state;
+
     return (
+      // this is not the right this
       <div className="vis">
-        {this.props.list.map(function (elem, i) {
-            return <ListElement key={i} i={elem} n={10}/>
-        })}
+        {this.props.list.map((elem, i) => <ListElement key={shortid.generate()} i={elem} n={max}/>)}
       </div>
     );
   }
